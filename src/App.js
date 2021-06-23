c012a85a9549bc90bccc742d2772464b64ae6d10
wpMcDonnell <will.mcdonnell4@gmail.com>
2021-06-22 20:01:01 -0400

    Fix app component to recieve JSON data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      web3: 'undefined',
+      account: '',
+      token: null,
+      dbank: null,
+      balance: 0,
+      dBankAddress: null
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          YO
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
+}
 }
 
 export default App;
