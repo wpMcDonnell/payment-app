dfcd805eb07df59735a5d3ce7198a90d412c9998
wpMcDonnell <will.mcdonnell4@gmail.com>
2021-06-23 11:53:36 -0400

    Add sass and dropdown button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import './App.css';
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
 const data = require ('./Data/sample.json')
 
 class App extends Component {
@@ -16,19 +18,29 @@ class App extends Component {
 
   render() {
     let dataJsx = <div>
-      {this.state.data.map(data =>
-      <p> {data.Payee.Name} </p> )}
+      {this.state.data.sort().map(data =>
+      <Dropdown.Item eventKey="1++">{data.Payee.Name}</Dropdown.Item>)}
     </div>
 
+    console.log(dataJsx)
+
   return (
+    <Fragment>
+    
     <div className="App">
       <header className="App-header">
         <div>
-          {dataJsx}
+        <DropdownButton id="dropdown-basic-button" variant="success" title="Vendors">
+        <Dropdown.Menu show>
+        <Dropdown.Header>Dropdown header</Dropdown.Header>
+        {dataJsx}
+        </Dropdown.Menu>
+        </DropdownButton>
         </div>
 
       </header>
     </div>
+    </Fragment>
   );
 }
 }
