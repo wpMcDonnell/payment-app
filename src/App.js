e7b7cbaaa923f8b81598b9977632bda27a0f8e12
wpMcDonnell <will.mcdonnell4@gmail.com>
2021-06-24 14:27:27 -0400

    Add extra div container for payee and card

--- src/App.js
+++ src/App.js
@@ -57,8 +57,9 @@ class App extends Component {
             </Card>
           </div>
           {/* Div / section for Payee information i.e. fax, address, phone -- main title card */}
-          <div className='d-flex container justify-content-between mb-4'>
-            <Card className='col-12 col-lg-6 shadow'>
+          <div className='d-flex container mb-4'>
+            <div className='col-12 col-lg-6'>
+            <Card className='col-12 shadow'>
               <div className='container mt-3 mb-1'>
                 <p className='payment-text'>Payee Info:</p>
                 <div className='key-text-header'> ATTN:
@@ -80,13 +81,12 @@ class App extends Component {
                 </div>
               </div>
             </Card>
-            </div>
+
 
 
 
           {/* Payment card object info listing selected json info */}
-            <div className='d-flex container mb-4'>
-            <Card className='col-12 col-lg-6 shadow'>
+            <Card className='col-12 shadow'>
             <div className='container mt-3 mb-1'>
               <div className='card-header-line'>
                 <p className='payment-text' id='payment-card-text'> Payment Card </p>
@@ -101,27 +101,30 @@ class App extends Component {
             </div>
             </Card>
           </div>
+          </div>
           {/* Start Remmittance section... title for array of payments */}
-          <h2 id='remit-title'> Remittance </h2>
-          {/* Payment card object info listing selected json info */}
-          {event.Remittance.map(remittance =>
-          <div key={remittance.InvoiceNo} className='d-flex container mb-3'>
-            <Card className='pan col-12 shadow'>
-            <div className='container mt-3 mb-2'>
-              <div className='card-header-line'>
-                <p className='payment-text'>{remittance.PayorName}</p>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-file-text" viewBox="0 0 16 16">
-                <path d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1H5z"/>
-                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z"/>
-                </svg>
-              </div>
-              <div className='key-text-header'> Amount: <p>{remittance.Amount}</p> </div>
-              <div className='key-text-header'> Inovice Number: <p>{remittance.InvoiceNo}</p> </div>
-              <div className='key-text-header'> Payor ID: <p>{remittance.PayorId}</p> </div>
-              <div className='key-text-header'> Description: <p>{remittance.Description}</p> </div>
-            </div>
-            </Card>
-          </div>)}
+          <div className='remittance-div-section'>
+            <h2 id='remit-title'> Remittance </h2>
+            {/* Payment card object info listing selected json info */}
+            {event.Remittance.map(remittance =>
+              <div key={remittance.InvoiceNo} className='d-flex container mb-3'>
+                <Card className='pan col-12 col-lg-5 shadow'>
+                <div className='container mt-3 mb-2'>
+                  <div className='card-header-line'>
+                    <p className='payment-text'>{remittance.PayorName}</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-file-text" viewBox="0 0 16 16">
+                    <path d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1H5z"/>
+                    <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z"/>
+                    </svg>
+                  </div>
+                  <div className='key-text-header'> Amount: <p>{remittance.Amount}</p> </div>
+                  <div className='key-text-header'> Inovice Number: <p>{remittance.InvoiceNo}</p> </div>
+                  <div className='key-text-header'> Payor ID: <p>{remittance.PayorId}</p> </div>
+                  <div className='key-text-header'> Description: <p>{remittance.Description}</p> </div>
+                </div>
+                </Card>
+            </div>)}
+          </div>
 
 
         </div>
