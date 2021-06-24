97e03a29cc372116105283913c0ca1be30e53458
wpMcDonnell <will.mcdonnell4@gmail.com>
2021-06-24 12:13:14 -0400

    Add card svg perfect layout

--- src/App.js
+++ src/App.js
@@ -87,7 +87,10 @@ class App extends Component {
           <div className='d-flex container mb-4'>
             <Card className='col-12 shadow'>
             <div className='container mt-3 mb-1'>
-              <p className='payment-text'>Payment Card</p>
+              <p className='payment-text' id='payment-card'> Payment Card <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="ml-5 bi bi-credit-card" viewBox="0 0 16 16">
+              <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1H2zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V7z"/>
+              <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1z"/>
+              </svg></p>
               <div className='key-text-header'> PAN: <p>{event.Payment.PAN}</p> </div>
               <div className='key-text-header'> Exp: <p>{event.Payment.Exp}</p> </div>
               <div className='key-text-header'> CVV: <p>{event.Payment.CVV}</p> </div>
